66d04dd3b2e9a1b0e0aef9e1907074a07d367f95
LanaAdekola <adekolaadelana@gmail.com>
2023-01-19 23:24:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-s
+buttonEl = document.getElementById("button-el")
+inputEl = document.getElementById("input-el")
+lengthEl = document.getElementById("length-el")
+
+const metertofeet = 3.281
+const literstogallons =0.264
+const kilogramstopounds = 2.204
+
+buttonEl.addEventListener("click", function (){
+     let displayvalue = inputEl.value
+     
+})
